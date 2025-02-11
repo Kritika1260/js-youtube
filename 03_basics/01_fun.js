ee9d2a5e6cdb2b13aa2140f70db13743639fe3a6
Kritika Chaturvedi <kritikachaturvedi0010@gmail.com>
2025-02-11 20:00:22 +0530

    Your commit message here

--- 03_basics/01_fun.js
+++ 03_basics/01_fun.js
@@ -29,7 +29,7 @@ function twoNum(number1, number2) //here we call parameters
 // twoNum(3, 4) //7
 // twoNum(3, "4") //34
 
-const result = twoNum(3, 5) //upper result and this result are different 
+// const result = twoNum(3, 5) //upper result and this result are different 
 // console.log(result);
 
 function loginUser(username) //we can also pass as username = "Kritika" in fun
@@ -41,5 +41,50 @@ function loginUser(username) //we can also pass as username = "Kritika" in fun
     }
     return `${username} just logged in` //string
 }
-console.log(loginUser("Kritika")); //op = Kritika just logged in
-console.log(loginUser()); //op = undefined
+// console.log(loginUser("Kritika")); //op = Kritika just logged in
+// console.log(loginUser()); //op = undefined
+
+// function calcPrice(num1)
+// {
+//     return num1;
+// }
+// console.log(calcPrice(2)); //op= 2
+
+// ...name this is called rest operator or spread operator on basis of usecase
+
+// function calcPrice(...num1) //rest operator
+// {
+//     return num1;
+// }
+// console.log(calcPrice(200, 400, 600)); //op = [ 200, 400, 600 ]
+
+function calcPrice(val1, val2,...num1) //rest operator
+{
+    return num1;
+}
+//imp for interview
+// console.log(calcPrice(200, 400, 600, 10000)); //op = [ 600, 10000 ]
+
+const user={
+    name:"Kritika",
+    price: 199
+}
+function handleObj(anyObj)
+{
+    console.log(`Username is ${anyObj.name} and price is ${anyObj.price}`);
+    
+}
+
+handleObj(user) //op = Username is Kritika and price is 199
+handleObj(
+{
+    name:"Sam",
+    price:399 //passing value in object
+})
+const newArr = [22, 33, 44]
+function returnSecVal(getArr)
+{
+    return getArr[1]
+}
+// console.log(returnSecVal(newArr));
+console.log([55, 88,99]);
